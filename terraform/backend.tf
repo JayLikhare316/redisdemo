4c787699c10c41915e95c7cb850f6676cd7d94d5
JayLikhare316 <likharjee1@gmail.com>
2025-06-25 19:35:11 +0530

    .

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "devopsninja"   
-    key            = "terraform/terraform.tfstate"     
-    region         = "ap-south-1"              
-    encrypt        = true
+# terraform {
+#   backend "s3" {
+#     bucket         = "JaybhaukiS3-terraform-state"   
+#     key            = "terraform/terraform.tfstate"     
+#     region         = "ap-south-1"              
+#     encrypt        = true
     
-  }
-}
+#   }
+# }
